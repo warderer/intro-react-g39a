99e335a808b626540e8baf72e9794ee510d0ad5f
César Guerra <warderer@gmail.com>
2025-05-01 21:24:43 -0600

    ✏️ Fix double react fragment at App

--- 02-react-props/src/App.jsx
+++ 02-react-props/src/App.jsx
@@ -12,150 +12,147 @@ import Info from './components/Info'
 function App () {
   return (
     <>
-      <>
-        <header className='hero'>
-          <div className='textos-hero'>
-            <h1>Bienvenido a Website</h1>
-            <p>Creamos el mejor sitio web para ti</p>
-            <a href='#contacto'>Contactame</a>
-          </div>
-          <div className='svg-hero' style={{ height: 150, overflow: 'hidden' }}>
-            <svg
-              viewBox='0 0 500 150'
-              preserveAspectRatio='none'
-              style={{ height: '100%', width: '100%' }}
-            >
-              <path
-                d='M0.00,49.98 C149.99,150.00 349.20,-49.98 500.00,49.98 L500.00,150.00 L0.00,150.00 Z'
-                style={{ stroke: 'none', fill: '#fff' }}
-              />
-            </svg>
-          </div>
-        </header>
+      <header className='hero'>
+        <div className='textos-hero'>
+          <h1>Bienvenido a Website</h1>
+          <p>Creamos el mejor sitio web para ti</p>
+          <a href='#contacto'>Contactame</a>
+        </div>
+        <div className='svg-hero' style={{ height: 150, overflow: 'hidden' }}>
+          <svg
+            viewBox='0 0 500 150'
+            preserveAspectRatio='none'
+            style={{ height: '100%', width: '100%' }}
+          >
+            <path
+              d='M0.00,49.98 C149.99,150.00 349.20,-49.98 500.00,49.98 L500.00,150.00 L0.00,150.00 Z'
+              style={{ stroke: 'none', fill: '#fff' }}
+            />
+          </svg>
+        </div>
+      </header>
 
-        <section className='wave-contenedor website'>
-          <img src={ilustracion1} alt='' />
+      <section className='wave-contenedor website'>
+        <img src={ilustracion1} alt='' />
+        <div className='contenedor-textos-main'>
+          <h2 className='titulo left'>Title of section</h2>
+          <p className='parrafo'>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Incidunt enim
+            reiciendis molestias nam tempore. Ullam hic accusantium eligendi ipsam
+            corrupti!
+          </p>
+          <a href='' className='cta'>
+            Learn more
+          </a>
+        </div>
+      </section>
+
+      <Info title='Juntos podemos apoyar' text='Lorem ipsum dolor sit amet consectetur adipisicing elit.' />
+
+      <section className='cards contenedor'>
+        <h2 className='titulo'>Our services</h2>
+        <div className='content-cards'>
+          <article className='card'>
+            <i className='far fa-clone' />
+            <h3>Title Card</h3>
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit.</p>
+            <a href='' className='cta'>
+              Learn more
+            </a>
+          </article>
+          <article className='card'>
+            <i className='fas fa-database' />
+            <h3>Title Card</h3>
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit.</p>
+            <a href='' className='cta'>
+              Learn more
+            </a>
+          </article>
+          <article className='card'>
+            <i className='far fa-object-group' />
+            <h3>Title Card</h3>
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit.</p>
+            <a href='' className='cta'>
+              Learn more
+            </a>
+          </article>
+        </div>
+      </section>
+
+      <Info title='Hola G39A' text='Estamos Aprendiendo React. ¡Vamos a echarle ganitas!' />
+
+      <section className='galeria'>
+        <div className='contenedor'>
+          <h2 className='titulo'>Our work</h2>
+          <article className='galeria-cont'>
+            <img src={uno} alt='' />
+            <img src={dos} alt='' />
+            <img src={tres} alt='' />
+            <img src={cuatro} alt='' />
+            <img src={cinco} alt='' />
+            <img src={seis} alt='' />
+          </article>
+        </div>
+      </section>
+      <section className='info-last'>
+        <div className='contenedor last-section'>
           <div className='contenedor-textos-main'>
             <h2 className='titulo left'>Title of section</h2>
             <p className='parrafo'>
-              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Incidunt enim
-              reiciendis molestias nam tempore. Ullam hic accusantium eligendi ipsam
-              corrupti!
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolorum
+              reprehenderit nostrum expedita quasi odio architecto laudantium sunt
+              nemo dicta atque?
             </p>
             <a href='' className='cta'>
               Learn more
             </a>
           </div>
-        </section>
-
-        <Info title='Juntos podemos apoyar' text='Lorem ipsum dolor sit amet consectetur adipisicing elit.' />
-
-        <section className='cards contenedor'>
-          <h2 className='titulo'>Our services</h2>
-          <div className='content-cards'>
-            <article className='card'>
-              <i className='far fa-clone' />
-              <h3>Title Card</h3>
-              <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit.</p>
-              <a href='' className='cta'>
-                Learn more
-              </a>
-            </article>
-            <article className='card'>
-              <i className='fas fa-database' />
-              <h3>Title Card</h3>
-              <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit.</p>
-              <a href='' className='cta'>
-                Learn more
-              </a>
-            </article>
-            <article className='card'>
-              <i className='far fa-object-group' />
-              <h3>Title Card</h3>
-              <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit.</p>
-              <a href='' className='cta'>
-                Learn more
-              </a>
-            </article>
-          </div>
-        </section>
-
-        <Info title='Hola G39A' text='Estamos Aprendiendo React. ¡Vamos a echarle ganitas!' />
-
-        <section className='galeria'>
-          <div className='contenedor'>
-            <h2 className='titulo'>Our work</h2>
-            <article className='galeria-cont'>
-              <img src={uno} alt='' />
-              <img src={dos} alt='' />
-              <img src={tres} alt='' />
-              <img src={cuatro} alt='' />
-              <img src={cinco} alt='' />
-              <img src={seis} alt='' />
-            </article>
-          </div>
-        </section>
-        <section className='info-last'>
-          <div className='contenedor last-section'>
-            <div className='contenedor-textos-main'>
-              <h2 className='titulo left'>Title of section</h2>
-              <p className='parrafo'>
-                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolorum
-                reprehenderit nostrum expedita quasi odio architecto laudantium sunt
-                nemo dicta atque?
-              </p>
-              <a href='' className='cta'>
-                Learn more
-              </a>
-            </div>
-            <img src={ilustracion} alt='' />
-          </div>
-          <div className='svg-wave' style={{ height: 150, overflow: 'hidden' }}>
-            <svg
-              viewBox='0 0 500 150'
-              preserveAspectRatio='none'
-              style={{ height: '100%', width: '100%' }}
-            >
-              <path
-                d='M0.00,49.98 C149.99,150.00 349.20,-49.98 500.00,49.98 L500.00,150.00 L0.00,150.00 Z'
-                style={{ stroke: 'none', fill: '#f5576c' }}
-              />
-            </svg>
-          </div>
-        </section>
-        <footer id='contacto'>
-          <div className='contenedor'>
-            <h2 className='titulo'>Contact us</h2>
-            <form action='' className='form'>
-              <input
-                className='input'
-                type='text'
-                name=''
-                id=''
-                placeholder='Nombre'
-              />
-              <input
-                className='input'
-                type='email'
-                name=''
-                id=''
-                placeholder='Email'
-              />
-              <textarea
-                className='input'
-                name=''
-                id=''
-                cols={30}
-                rows={10}
-                placeholder='Mensaje'
-                defaultValue=''
-              />
-              <input className='input' type='submit' defaultValue='Enviar' />
-            </form>
-          </div>
-        </footer>
-      </>
-
+          <img src={ilustracion} alt='' />
+        </div>
+        <div className='svg-wave' style={{ height: 150, overflow: 'hidden' }}>
+          <svg
+            viewBox='0 0 500 150'
+            preserveAspectRatio='none'
+            style={{ height: '100%', width: '100%' }}
+          >
+            <path
+              d='M0.00,49.98 C149.99,150.00 349.20,-49.98 500.00,49.98 L500.00,150.00 L0.00,150.00 Z'
+              style={{ stroke: 'none', fill: '#f5576c' }}
+            />
+          </svg>
+        </div>
+      </section>
+      <footer id='contacto'>
+        <div className='contenedor'>
+          <h2 className='titulo'>Contact us</h2>
+          <form action='' className='form'>
+            <input
+              className='input'
+              type='text'
+              name=''
+              id=''
+              placeholder='Nombre'
+            />
+            <input
+              className='input'
+              type='email'
+              name=''
+              id=''
+              placeholder='Email'
+            />
+            <textarea
+              className='input'
+              name=''
+              id=''
+              cols={30}
+              rows={10}
+              placeholder='Mensaje'
+              defaultValue=''
+            />
+            <input className='input' type='submit' defaultValue='Enviar' />
+          </form>
+        </div>
+      </footer>
     </>
   )
 }
