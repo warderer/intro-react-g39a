c66b6acafe40f69bd2f1fd50c88b696b2723c3a1
César Guerra <warderer@gmail.com>
2025-05-06 20:53:35 -0600

    ✨ Add inputValue State and Add button

--- 03-todo-list/src/App.jsx
+++ 03-todo-list/src/App.jsx
@@ -1,11 +1,26 @@
+import { useState } from 'react'
 import './App.css'
 
 function App () {
+  /* 1. Creo el estado donde guardare la información del input */
+  const [inputValue, setInputValue] = useState('')
+
+  /* 3. Creo una función que se ejecuta cuando se hace click al boton agregar */
+  const handleAdd = () => {
+    console.log('Agregue:', inputValue)
+  }
+
   return (
     <>
       <h1>Lista de Tareas</h1>
-      <input type='text' placeholder='Escribe una tarea' />
-      <button>Agregar</button>
+      {/* 2. Usar un evento onChange para guardar la información del estado, apenas esta sea tecleada. La información del input la obtenemos de event.target.value */}
+      <input
+        type='text'
+        placeholder='Escribe una tarea'
+        value={inputValue}
+        onChange={(event) => setInputValue(event.target.value)}
+      />
+      <button onClick={handleAdd}>Agregar</button>
       {/* ul>li{Item $}*3>button{Eliminar} */}
       <ul>
         <li>Item 1<button>Eliminar</button></li>
