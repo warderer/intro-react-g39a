032bc89ef245d8c63cd902d109dae7e86f2e9fb5
César Guerra <warderer@gmail.com>
2025-05-08 21:06:47 -0600

    ✨ call to API trends - useEffect

--- 04-giphy-app/src/App.jsx
+++ 04-giphy-app/src/App.jsx
@@ -1,10 +1,25 @@
+import { useState, useEffect } from 'react'
 import './App.css'
 
-function App() {
+function App () {
+  const [gifs, setGifs] = useState([])
+  const apiKey = import.meta.env.VITE_GIPHY_API_KEY
+
+  useEffect(() => {
+    const fetchGifs = async () => {
+      const response = await fetch(
+        `https://api.giphy.com/v1/gifs/trending?api_key=${apiKey}&limit=25&offset=0&rating=g&bundle=messaging_non_clips`
+      )
+      const results = await response.json()
+      setGifs(results.data)
+    }
+
+    fetchGifs()
+  }, [])
 
   return (
     <>
-
+      <h1>Giphy App</h1>
     </>
   )
 }
