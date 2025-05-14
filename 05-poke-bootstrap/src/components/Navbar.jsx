c800d3e71b81c62dd554a7cbef626a985863808f
César Guerra <warderer@gmail.com>
2025-05-13 21:09:07 -0600

    ✨ Add Links to Navbar with NavLink

--- 05-poke-bootstrap/src/components/Navbar.jsx
+++ 05-poke-bootstrap/src/components/Navbar.jsx
@@ -1,10 +1,12 @@
+import { NavLink } from 'react-router-dom'
+
 const Navbar = () => {
   return (
     <nav className='navbar navbar-expand-lg bg-warning'>
       <div className='container-fluid'>
-        <a className='navbar-brand' href='#'>
+        <NavLink className='navbar-brand' to='/'>
           Pokédex
-        </a>
+        </NavLink>
         <button
           className='navbar-toggler'
           type='button'
@@ -18,15 +20,15 @@ const Navbar = () => {
         </button>
         <div className='collapse navbar-collapse' id='navbarNavAltMarkup'>
           <div className='navbar-nav'>
-            <a className='nav-link active' aria-current='page' href='#'>
+            <NavLink className='nav-link active' aria-current='page' to='/'>
               Home
-            </a>
-            <a className='nav-link' href='#'>
+            </NavLink>
+            <NavLink className='nav-link' to='/about'>
               About
-            </a>
-            <a className='nav-link' href='#'>
+            </NavLink>
+            <NavLink className='nav-link' to='/contact'>
               Contact
-            </a>
+            </NavLink>
           </div>
         </div>
       </div>
