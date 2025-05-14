10e8434323058963e4d4a0e274a32bf82cac3628
César Guerra <warderer@gmail.com>
2025-05-13 19:33:58 -0600

    💄 Add bootstrap UI to Navbar

--- 05-poke-bootstrap/src/components/Navbar.jsx
+++ 05-poke-bootstrap/src/components/Navbar.jsx
@@ -1,6 +1,37 @@
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <nav className='navbar navbar-expand-lg bg-warning'>
+      <div className='container-fluid'>
+        <a className='navbar-brand' href='#'>
+          Pokédex
+        </a>
+        <button
+          className='navbar-toggler'
+          type='button'
+          data-bs-toggle='collapse'
+          data-bs-target='#navbarNavAltMarkup'
+          aria-controls='navbarNavAltMarkup'
+          aria-expanded='false'
+          aria-label='Toggle navigation'
+        >
+          <span className='navbar-toggler-icon' />
+        </button>
+        <div className='collapse navbar-collapse' id='navbarNavAltMarkup'>
+          <div className='navbar-nav'>
+            <a className='nav-link active' aria-current='page' href='#'>
+              Home
+            </a>
+            <a className='nav-link' href='#'>
+              About
+            </a>
+            <a className='nav-link' href='#'>
+              Contact
+            </a>
+          </div>
+        </div>
+      </div>
+    </nav>
+
   )
 }
 export default Navbar
